4924970524a173ae29287a34180381fe99272fbf
Tamas Szabo <szabo.tamas@i-host.hu>
2017-05-04 00:13:23 +0200

    Added the SpiralTable class refact

--- app/src/main/java/hu/i_host/thespiralkata/SpiralTable.java
+++ app/src/main/java/hu/i_host/thespiralkata/SpiralTable.java
@@ -1,58 +1,55 @@
 package hu.i_host.thespiralkata;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import android.util.Log;
+
+import java.security.InvalidParameterException;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
 
-/**
- * Created by haka on 2017. 04. 26..
- */
-
 public class SpiralTable {
-    SpiralLine     mLine;
-    List<String>   mDirectionList        = new ArrayList<>();
-    Iterator       mDirectionListIterator;
-    private String mDirection;
+    private List<String> mDirectionList;
+    private SpiralLine mLine;
 
-    public SpiralTable(SpiralLine line) {
-        mLine = line;
-        mDirectionList.add("right");
-        mDirectionList.add("down");
-        mDirectionList.add("left");
-        mDirectionList.add("up");
+    private int[][] mMatrix;
+    private Iterator mDirectionListIterator;
+
+    @Inject
+    public SpiralTable(SpiralLine line, List<String> directionList) {
+        mDirectionList         = directionList;
+        mLine                  = line;
+        mDirectionListIterator = directionList.iterator();
     }
 
     public int[][] go(int dimension) {
-        // kettesevel novekszunk, kiveve az elsot. Ezek a zsmaok azt mutatjak, elotte mennyi olyan sor van, amit nem erint, illetve mogotte menni van az adott lepesnek.
-        // tehat (1 + 0 elso),
-        // 1 + 1, 1 + 1
-        // 3 + 1, 3 + 1
-        // 3 + 3, 3 + 3
-        // 5 + 3, 5 + 3  ha a dimensiobol kivonjuk az osszeget es az kisebb vagy egyenlo, mint a kovetkezo szam, akkor vege
 
-        mDirectionListIterator = mDirectionList.iterator();
+        if (dimension < 5) {
+            throw new InvalidParameterException("The dimension minimal size is 5.");
+        }
 
-        // ciklusban
-        int[][] matrix = new int[dimension][dimension];
+        mMatrix = new int[dimension][dimension];
 
-        fillUpMatrix(dimension, matrix);
+        fillUpMatrix(dimension);
 
         int stripeBefore = 0;
         int stripeAfter  = 1;
 
-        drawLine(dimension, stripeBefore, stripeAfter, matrix);
+        // Stripes 0+1
+        drawLine(dimension, stripeBefore, stripeAfter);
+
+        // Set the next stripe (1+1)
         stripeBefore = 1;
 
-        // egyesevel lepgetetunk csak majd kettesevel adjuk hozza
+        // Travers the stripes (1+1, 1+1, 1+3, 1+3, 3+3, 3+3, 3+5, ...)
+        // We know the length of the line from the stripes and the dimension 9 = 10 - (0 + 1)
         do {
+            // Whe have twister stripe pairs (1+3, 1+3)
             for (int k = 0; k < 2; k++) {
-                drawLine(dimension, stripeBefore, stripeAfter, matrix);
+                drawLine(dimension, stripeBefore, stripeAfter);
             }
 
-            // We would like to step like this: 0+1, 1+1, 3+1,3+3,5+3,5+5
+            // We would like to step like this: 1+1, 3+1, 3+3, 5+3, 5+5
             if (stripeBefore == stripeAfter) {
                 stripeBefore += 2;
             } else {
@@ -61,22 +58,18 @@ public class SpiralTable {
         }
         while (stripeAfter + stripeBefore + 1 < dimension);
 
-        return matrix;
+        return mMatrix;
     }
 
-    private void fillUpMatrix(int dimension, int[][] matrix) {
+    private void fillUpMatrix(int dimension) {
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
-                matrix[i][j] = 1;
+                mMatrix[i][j] = 1;
             }
         }
     }
 
-    /**
-     * Returns the next direction. It will loop the list.
-     *
-     * @return
-     */
+    /** Returns the next direction. It will loop the list. */
     private String getNextDirection() {
         if (!mDirectionListIterator.hasNext()) {
             mDirectionListIterator = mDirectionList.iterator();
@@ -85,35 +78,36 @@ public class SpiralTable {
         return (String)mDirectionListIterator.next();
     }
 
-    private void drawLine(int dimension, int stripeBefore, int stripeAfter, int[][] matrix) {
-        mDirection = getNextDirection();
+    private void drawLine(int dimension, int stripeBefore, int stripeAfter) {
         mLine.setLength(dimension - (stripeBefore + stripeAfter));
 
-        switch (mDirection) {
+        String direction = getNextDirection();
+        Log.e("Spiral", direction + " " + stripeBefore + " + " + stripeAfter);
+
+        switch (direction) {
             case "left":
-                // kivonjuk a mogotte levot a teljes dimensiobol és akkor megkapjuk honnan indul el balra pl
-                // 3 + 1 eseten 10 es dimensional 9 ről indul vissza
                 // It is always 1+1 or 3+3 or 5+5 so on
-                mLine.left(matrix);
+                mLine.left(mMatrix);
                 break;
 
             case "right":
                 // It is always 0+1 or 1+3 or 3+5 so on
-                mLine.right(matrix);
+                mLine.right(mMatrix);
                 break;
 
             case "down":
                 // It is always 1+1 or 3+3 or 5+5 so on
-                mLine.down(matrix);
+                mLine.down(mMatrix);
                 break;
             case "up":
                 // It is always 1+3 or 3+5 so on
-                mLine.up(matrix);
+                mLine.up(mMatrix);
                 break;
         }
     }
 
-    public void reset() {
-        mLine.reset();
+    void init() {
+        mDirectionListIterator = mDirectionList.iterator();
+        mLine.init();
     }
 }
